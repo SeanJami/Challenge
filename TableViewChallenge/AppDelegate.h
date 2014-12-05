0f5855f9bcf05af2c7118a9e6be2974dac02dd16
Marcel <marcel@Marcels-MacBook-Pro.local>
2014-12-06 00:18:22 +0100

    Initial Commit

---
+++ TableViewChallenge/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewChallenge
+//
+//  Created by Marcel on 06.12.14.
+//  Copyright (c) 2014 Marcel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewChallenge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableViewChallenge
+//
+//  Created by Marcel on 06.12.14.
+//  Copyright (c) 2014 Marcel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
